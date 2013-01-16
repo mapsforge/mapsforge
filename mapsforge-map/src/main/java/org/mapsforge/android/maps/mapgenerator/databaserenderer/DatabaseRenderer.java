0f71a427bd50cc273d720058ebfa3b544eac7f4e
Ludwig M Brinckmann <ludwigbrinckmann@gmail.com>
2013-01-16 12:46:46 +0800

    fix for issue397 : TextScale not applied when changing themes

--- mapsforge-map/src/main/java/org/mapsforge/android/maps/mapgenerator/databaserenderer/DatabaseRenderer.java
+++ mapsforge-map/src/main/java/org/mapsforge/android/maps/mapgenerator/databaserenderer/DatabaseRenderer.java
@@ -156,6 +156,9 @@ public class DatabaseRenderer implements RenderCallback {
 			createWayLists();
 			this.previousJobTheme = jobTheme;
 			this.previousZoomLevel = Byte.MIN_VALUE;
+			// invalidate the previousTextScale so that textScale from jobParameters will
+			// be applied next time
+			this.previousTextScale = -1;
 		}
 
 		byte zoomLevel = this.currentTile.zoomLevel;
