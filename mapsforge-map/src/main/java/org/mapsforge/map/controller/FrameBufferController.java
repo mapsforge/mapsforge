cfe5ddf1f09638d414f698ad3bf0d82b755f8835
jjbross <jjbross@gmail.com>
2013-06-11 16:57:35 +0200

    - changed copyright header in Java sources to cover the year 2013 - updated the Eclipse template that adds the copyright header to every new Java source file

--- mapsforge-map/src/main/java/org/mapsforge/map/controller/FrameBufferController.java
+++ mapsforge-map/src/main/java/org/mapsforge/map/controller/FrameBufferController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010, 2011, 2012 mapsforge.org
+ * Copyright 2010, 2011, 2012, 2013 mapsforge.org
  *
  * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free Software
