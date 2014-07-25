431910ddca073740fecb603f5655528aa9e27210
Emux <devemux86@gmail.com>
2014-07-25 11:12:39 +0300

    RenderThemeHandler: enable xml encoding detection from kXML

--- mapsforge-map/src/main/java/org/mapsforge/map/rendertheme/rule/RenderThemeHandler.java
+++ mapsforge-map/src/main/java/org/mapsforge/map/rendertheme/rule/RenderThemeHandler.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2010, 2011, 2012, 2013 mapsforge.org
  * Copyright 2014 Ludwig M Brinckmann
+ * Copyright 2014 devemux86
  *
  * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free Software
@@ -17,13 +18,12 @@ package org.mapsforge.map.rendertheme.rule;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.Stack;
 import java.util.logging.Logger;
 
-
+import org.kxml2.io.KXmlParser;
 import org.mapsforge.core.graphics.GraphicFactory;
 import org.mapsforge.core.util.IOUtils;
 import org.mapsforge.map.model.DisplayModel;
@@ -38,9 +38,8 @@ import org.mapsforge.map.rendertheme.renderinstruction.LineSymbol;
 import org.mapsforge.map.rendertheme.renderinstruction.PathText;
 import org.mapsforge.map.rendertheme.renderinstruction.RenderInstruction;
 import org.mapsforge.map.rendertheme.renderinstruction.Symbol;
-
-import org.kxml2.io.*;
-import org.xmlpull.v1.*;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 /**
  * KXML handler to parse XML render theme files.
@@ -63,7 +62,7 @@ public final class RenderThemeHandler {
 		InputStream inputStream = null;
 		try {
 			inputStream = xmlRenderTheme.getRenderThemeAsStream();
-			pullParser.setInput(new InputStreamReader(inputStream));
+			pullParser.setInput(inputStream, null);
 			renderThemeHandler.processRenderTheme();
 			renderThemeHandler.renderTheme.incrementRefCount();
 			return renderThemeHandler.renderTheme;
