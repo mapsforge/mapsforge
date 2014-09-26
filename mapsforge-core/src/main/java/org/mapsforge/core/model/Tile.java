8ebd2b5fce5e0954d946c948fd0e697c65344bae
Ludwig M Brinckmann <ludwigbrinckmann@gmail.com>
2014-09-26 21:33:46 +0300

    Tile: correct bbox computations at all zoomlevels

--- mapsforge-core/src/main/java/org/mapsforge/core/model/Tile.java
+++ mapsforge-core/src/main/java/org/mapsforge/core/model/Tile.java
@@ -128,10 +128,10 @@ public class Tile implements Serializable {
 	 */
 	public BoundingBox getBoundingBox() {
 		if (this.boundingBox == null) {
-			double minLatitude = MercatorProjection.tileYToLatitude(this.getBelow().tileY, zoomLevel);
-			double minLongitude = MercatorProjection.tileXToLongitude(this.tileX, zoomLevel);
-			double maxLatitude = MercatorProjection.tileYToLatitude(this.tileY, zoomLevel);
-			double maxLongitude = MercatorProjection.tileXToLongitude(this.getRight().tileX, zoomLevel);
+			double minLatitude = Math.max(MercatorProjection.LATITUDE_MIN, MercatorProjection.tileYToLatitude(tileY + 1, zoomLevel));
+			double minLongitude = Math.max(-180, MercatorProjection.tileXToLongitude(this.tileX, zoomLevel));
+			double maxLatitude = Math.min(MercatorProjection.LATITUDE_MAX, MercatorProjection.tileYToLatitude(this.tileY, zoomLevel));
+			double maxLongitude = Math.min(180, MercatorProjection.tileXToLongitude(tileX + 1, zoomLevel));
 			if (maxLongitude == -180) {
 				// fix for dateline crossing, where the right tile starts at -180 and causes an invalid bbox
 				maxLongitude = 180;
