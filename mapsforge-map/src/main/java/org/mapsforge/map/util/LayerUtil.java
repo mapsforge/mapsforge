8cec589bc55c60b4d3e62300f7c752f5ead1686f
Ludwig M Brinckmann <ludwigbrinckmann@gmail.com>
2014-10-03 21:24:20 +0300

    Code quality: fixed some sonar violations, no functionality change

--- mapsforge-map/src/main/java/org/mapsforge/map/util/LayerUtil.java
+++ mapsforge-map/src/main/java/org/mapsforge/map/util/LayerUtil.java
@@ -38,7 +38,7 @@ public final class LayerUtil {
 		int tileRight = MercatorProjection.longitudeToTileX(boundingBox.maxLongitude, zoomLevel);
 		int tileBottom = MercatorProjection.latitudeToTileY(boundingBox.minLatitude, zoomLevel);
 
-		int initialCapacity = (int) ((tileRight - tileLeft + 1) * (tileBottom - tileTop + 1));
+		int initialCapacity = (tileRight - tileLeft + 1) * (tileBottom - tileTop + 1);
 		List<TilePosition> tilePositions = new ArrayList<TilePosition>(initialCapacity);
 
 		for (int tileY = tileTop; tileY <= tileBottom; ++tileY) {
