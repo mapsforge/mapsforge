baab498018cad0f13c232d264ca482c700fad996
Ludwig M Brinckmann <ludwigbrinckmann@gmail.com>
2014-12-23 11:53:06 +0100

    Rendertheme Handling: rendertheme parsing now done as a future, moving it off the UI thread
    (cherry picked from commit 158e120e808547f46578e1e4c4a07fe93bb7f3c6)

---
+++ mapsforge-map/src/main/java/org/mapsforge/map/rendertheme/rule/RenderThemeFuture.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 Ludwig M Brinckmann
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mapsforge.map.rendertheme.rule;
+
+import org.mapsforge.core.graphics.GraphicFactory;
+import org.mapsforge.map.model.DisplayModel;
+import org.mapsforge.map.rendertheme.XmlRenderTheme;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * A RenderThemeFuture implements a asynchronous parsing of an XmlRenderTheme in order to
+ * move the delay caused by parsing the XML file off the user interface thread in mapsforge
+ * application.
+ */
+public class RenderThemeFuture extends FutureTask<RenderTheme> {
+
+	/**
+	 * Callable that performs the actual parsing of the render theme (via the RenderThemeHandler
+	 * as before).
+	 */
+	private static class RenderThemeCallable implements Callable<RenderTheme> {
+		private final GraphicFactory graphicFactory;
+		private final XmlRenderTheme xmlRenderTheme;
+		private final DisplayModel displayModel;
+
+		public RenderThemeCallable(GraphicFactory graphicFactory, XmlRenderTheme xmlRenderTheme, DisplayModel displayModel) {
+			this.graphicFactory = graphicFactory;
+			this.xmlRenderTheme = xmlRenderTheme;
+			this.displayModel = displayModel;
+		}
+
+		public RenderTheme call() {
+			if (xmlRenderTheme == null || this.displayModel == null) {
+				return null;
+			}
+			try {
+				return RenderThemeHandler.getRenderTheme(this.graphicFactory, displayModel, this.xmlRenderTheme);
+			} catch (XmlPullParserException e) {
+				throw new IllegalArgumentException("Parse error for XML rendertheme", e);
+			} catch (IOException e) {
+				throw new IllegalArgumentException("File error for XML rendertheme", e);
+			}
+		}
+	}
+
+	public RenderThemeFuture(GraphicFactory graphicFactory, XmlRenderTheme xmlRenderTheme, DisplayModel displayModel) {
+		super(new RenderThemeCallable(graphicFactory, xmlRenderTheme, displayModel));
+	}
+
+}
