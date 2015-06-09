0e6bd946b4374039a4151e9e4eab133713a4f326
Emux <devemux86@gmail.com>
2015-06-09 20:29:02 +0300

    TouchGestureDetector: remove obsolete zoomOut call, fix #616

--- mapsforge-map-android/src/main/java/org/mapsforge/map/android/input/TouchGestureDetector.java
+++ mapsforge-map-android/src/main/java/org/mapsforge/map/android/input/TouchGestureDetector.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2010, 2011, 2012, 2013 mapsforge.org
  * Copyright 2014 Jordan Black
+ * Copyright 2015 devemux86
  *
  * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free Software
@@ -101,7 +102,6 @@ public class TouchGestureDetector implements TouchEventListener {
 		long doubleTouchTime = eventTime - this.lastEventTime;
 		if (doubleTouchTime < this.gestureTimeout) {
 			this.lastActionUpPoint = null;
-			this.mapView.getModel().mapViewPosition.zoomOut();
 		}
 	}
 
