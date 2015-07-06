fc6da7ee3aab2182a0b9a6b68e1ca8a70760fc1f
Emux <devemux86@gmail.com>
2015-07-06 14:31:01 +0300

    MapWorkerPool: enable LabelLayer redraw, fix #642

--- mapsforge-map/src/main/java/org/mapsforge/map/layer/renderer/MapWorkerPool.java
+++ mapsforge-map/src/main/java/org/mapsforge/map/layer/renderer/MapWorkerPool.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2010, 2011, 2012, 2013 mapsforge.org
  * Copyright 2014 Ludwig M Brinckmann
+ * Copyright 2015 devemux86
  *
  * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free Software
@@ -123,8 +124,8 @@ public class MapWorkerPool implements Runnable {
 				if (bitmap != null) {
 					MapWorkerPool.this.tileCache.put(rendererJob, bitmap);
 					MapWorkerPool.this.databaseRenderer.removeTileInProgress(rendererJob.tile);
-					MapWorkerPool.this.layer.requestRedraw();
 				}
+				MapWorkerPool.this.layer.requestRedraw();
 
 				if (DEBUG_TIMING) {
 					long end = System.currentTimeMillis();
