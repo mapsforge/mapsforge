be1877e12860de6574f0aec60edc32a34b485ef8
nickw1 <nick_whitelegg@yahoo.co.uk>
2015-08-31 16:14:20 +0100

    Now deals with GeoJSON multilinestrings and multipolygons correctly; removed MapActivity

--- docs/Getting-Started-GeoJSON.md
+++ docs/Getting-Started-GeoJSON.md
@@ -7,12 +7,13 @@ example below compiles and runs and shows a (rather messy) map.
 
     package freemap.mapsforgebasic;
     
+    import android.app.Activity;
     import android.os.Bundle;
     import android.os.Environment;
   
-    import org.mapsforge.map.android.MapActivity;  
     import org.mapsforge.map.android.util.AndroidUtil;
     import org.mapsforge.map.android.view.MapView;
+    import org.mapsforge.map.layer.cache.TileCache;
     import org.mapsforge.map.layer.renderer.TileRendererLayer;
     import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
     import org.mapsforge.map.reader.GeoJSONDataSource;
@@ -31,19 +32,22 @@ example below compiles and runs and shows a (rather messy) map.
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            AndroidGraphicFactory.createInstance(this.getApplication());
     
             mv = new MapView(this);
     
             setContentView(mv);
             mv.setBuiltInZoomControls(true);
             mv.setClickable(true);
-    
-            createDefaultTileCache(mv, "mapcache");
-    
+  
+            tileCache = AndroidUtil.createTileCache (this, "mapcache",
+                mv.getModel().displayModel.getTileSize(), 1f,
+                mv.getModel().frameBufferModel,getOverdrawFactor());
+ 
             DownloadCache downloadCache = AndroidUtil.getDownloadCache (this, "gjtest");
     
             ds = new GeoJSONDataSource("http://www.free-map.org.uk/fm/ws/tsvr.php",
-                    "way=highway,natural&poi=natural,place,amenity&ext=20&contour=1&outProj=4326");
+                    "way=highway,natural,waterway&poi=natural,place,amenity&ext=20&contour=1&outProj=4326");
     
     
             ds.setDownloadCache(downloadCache);
@@ -58,8 +62,8 @@ example below compiles and runs and shows a (rather messy) map.
                 mv.setZoomLevel((byte) 14);
     
     
-                layer = new TileRendererLayer(getTileCache(), ds,
-                        mv.getModel().mapViewPosition, false, true,
+                layer = new TileRendererLayer(tileCache, ds,
+                        mv.getModel().mapViewPosition, 
                         AndroidGraphicFactory.INSTANCE);
     
                 ExternalRenderTheme theme = new ExternalRenderTheme
@@ -97,11 +101,7 @@ Note the following on this example:
 
 The example also shows the following Android Mapsforge API tweaks for this
 fork of the repository:
-* We use MapActivity for our activity. This takes care of some commonly-used
-  tasks for us, e.g. cache management, simplifying the code:
-    * its onCreate() calls AndroidGraphicFactory.createInstance();
-    * it has a convenience createDefaultTileCache() method which creates a
-    tile cache with default settings given a MapView. We can then access the 
-    cache with getTileCache() (e.g. when we create our TileRendererLayer);
 * MapView has convenience methods setCenter(), setZoomLevel() and addLayer()
   added, simplifying the API.
+* TileRendererLayer has an additional 4-parameter constructor which assumes
+  default values for transparency (false) and whether to render labels (true).
--- mapsforge-map-reader/src/main/java/org/mapsforge/map/reader/GeoJSONReader.java
+++ mapsforge-map-reader/src/main/java/org/mapsforge/map/reader/GeoJSONReader.java
@@ -16,7 +16,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class GeoJSONReader {
-    
+
+	static class FeatureTests {
+		public static boolean isWaterFeature(String k, String v) {
+			return k.equals("natural") && v.equals("water") ||
+					k.equals("waterway");
+		}
+	
+		public static boolean isLandscapeFeature(String k, String v) {
+			return k.equals("natural") && v.equals("wood") ||
+					k.equals("landuse") && v.equals("forest") ||
+					k.equals("natural") && v.equals("heath");
+		}
+	}    
+
     public GeoJSONReader() {
     }
 
@@ -35,40 +48,56 @@ public class GeoJSONReader {
         JSONObject data = new JSONObject(jsonString);
         JSONArray features = data.getJSONArray("features");
         System.out.println("Read " + features.length()+" features from JSON.");
+		byte layer;
         for    (int i=0; i<features.length(); i++) {
             JSONObject currentFeature = features.getJSONObject(i);
             String type = currentFeature.getString("type");
+			layer=(byte)4; // default for roads, paths etc
             if(type.equals("Feature")) {
-                JSONObject    geometry = currentFeature.getJSONObject("geometry"),
+                JSONObject geometry = currentFeature.getJSONObject("geometry"),
                     properties = currentFeature.getJSONObject("properties");
                 ArrayList<Tag> tags = new ArrayList<Tag>();    
                 Iterator it = properties.keys();
                 while(it.hasNext()) {
-                    String k = (String)it.next();
-                    tags.add(new Tag(k,properties.getString(k)));                
+                    String k = (String)it.next(), v=properties.getString(k);
+					if(k.equals("contour")) {
+						layer=(byte)2; // contours under roads/paths
+					} else if (FeatureTests.isLandscapeFeature(k,v)) {
+						layer = (byte)1; // woods etc below contours
+					} else if (FeatureTests.isWaterFeature(k,v)) {
+						layer = (byte)3; // lakes above contours, below roads
+					}
+                    tags.add(new Tag(k,v));                
                 }
 
                 String gType = geometry.getString("type");
+                JSONArray coords = geometry.getJSONArray("coordinates");
                 if(gType.equals("Point")) {
-                    JSONArray coords = geometry.getJSONArray("coordinates");
                     LatLong ll = new LatLong
                         ( coords.getDouble(1), coords.getDouble(0) );
                     PointOfInterest poi = new PointOfInterest
-                        ((byte)2, tags, ll);
+                        ((byte)5, tags, ll); // pois above all else
                     bundle.pois.add(poi);
                 } else if (gType.equals("LineString")) {
-                    JSONArray coords = geometry.getJSONArray("coordinates");
-                    LatLong[][] points = new LatLong[1][];
-                    points[0] = new LatLong[coords.length()];
-                    for(int j=0; j<coords.length(); j++) {
-                        JSONArray curCoords = coords.getJSONArray(j);
-                        points[0][j] = 
-                            new LatLong(curCoords.getDouble(1),
-                                        curCoords.getDouble(0));
-                    }    
-                    Way way = new Way((byte)1, tags, points, null);
+                    LatLong[][] points = readWayFeature(coords);
+                    Way way = new Way(layer, tags, points, null);
                     bundle.ways.add(way);
-                }    
+                } else if (gType.equals("MultiLineString")) {
+                    LatLong[][] points = readMultiWayFeature(coords);
+                    Way way = new Way(layer, tags, points, null);
+                    bundle.ways.add(way);
+                } else if (gType.equals("Polygon")) {
+                    // polygons are 3-deep in geojson but only actually 
+                    // contain one line so we simplify them
+                    LatLong[][] points = readWayFeature(coords.getJSONArray(0));
+                    Way way = new Way(layer, tags, points, null);
+                    bundle.ways.add(way);
+                } else if (gType.equals("MultiPolygon")) {
+                    LatLong[][] points=readMultiWayFeature
+                        (coords.getJSONArray(0));
+                    Way way = new Way(layer, tags, points, null);
+                    bundle.ways.add(way);
+                } 
             }
         }
         return bundle;
@@ -84,5 +113,30 @@ public class GeoJSONReader {
             text.append(new String(bytes,0,bytesRead));
         }
         return text.toString();
+    }
+
+
+    private LatLong[][] readWayFeature(JSONArray coords) {
+        LatLong[][] points = new LatLong[1][];
+        points[0] = new LatLong[coords.length()];
+        readLineString(points[0], coords);
+        return points;
+    }
+
+    private void readLineString(LatLong[] points, JSONArray coords) {
+        for(int j=0; j<coords.length(); j++) {
+            JSONArray curPoint = coords.getJSONArray(j);
+            points[j] = new LatLong(curPoint.getDouble(1),
+                                        curPoint.getDouble(0));
+        }    
+    }
+
+    private LatLong[][] readMultiWayFeature (JSONArray coords) {
+        LatLong[][] points = new LatLong[coords.length()][];    
+        for(int i=0; i<coords.length(); i++) {
+            points[i]=new LatLong[coords.getJSONArray(i).length()];
+            readLineString(points[i], coords.getJSONArray(i));
+        }
+        return points;
     }
 }
