f18bb5e908400a48acb0e580609fbd52533de50a
Martin Vennekamp (Dickschiff) <vennekamp@googlemail.com>
2015-10-10 13:44:54 +0200

    Licence changed.

--- Applications/Android/Samples/src/main/java/org/mapsforge/applications/android/samples/dummy/ManyDummyContent.java
+++ Applications/Android/Samples/src/main/java/org/mapsforge/applications/android/samples/dummy/ManyDummyContent.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2009 Huan Erdao
+ * Copyright (C) 2014 Martin Vennekamp/*
+ * Copyright 2015 mapsforge.org
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.mapsforge.applications.android.samples.dummy;
 
 import android.os.AsyncTask;
@@ -20,9 +36,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by martin on 07.12.2014.
- */
 public class ManyDummyContent extends DummyContent {
 
     private static final String TAG = ManyDummyContent.class.getSimpleName();
