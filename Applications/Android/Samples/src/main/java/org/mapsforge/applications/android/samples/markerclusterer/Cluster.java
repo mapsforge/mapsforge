6498dfc9b15a73f870e1a3653def617fd80cfbde
Martin Vennekamp (Dickschiff) <vennekamp@googlemail.com>
2015-10-10 19:47:32 +0200

    Clean Commit of Cluster-Algorithm.

---
+++ Applications/Android/Samples/src/main/java/org/mapsforge/applications/android/samples/markerclusterer/Cluster.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2009 Huan Erdao
+ * Copyright (C) 2014 Martin Vennekamp
+ * Copyright 2015 mapsforge.org
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mapsforge.applications.android.samples.markerclusterer;
+
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.layer.Layers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Cluster class.
+ * contains single marker object(ClusterMarker). mostly wraps methods in ClusterMarker.
+ */
+public class Cluster<T extends GeoItem> {
+    /**	GeoClusterer object	 */
+    private ClusterManager<T> mClusterManager;
+    public ClusterManager<T> getClusterManager(){
+    	return mClusterManager;
+    }
+    /**	center of cluster */
+    private LatLong mCenter;
+    /**	list of GeoItem within cluster */
+    private List<T> mItems = Collections.synchronizedList(new ArrayList<T>());
+    /** ClusterMarker object */
+    private ClusterMarker<T> mClusterMarker;
+
+	public String getTitle() {
+		if ( getItems().size() == 1 ) {
+			return getItems().get(0).getTitle();
+		}
+		return String.valueOf(getItems().size());
+	}
+    /**
+     * @param clusterer ClusterManager object.
+     */
+    public Cluster(ClusterManager<T> clusterer, T item) {
+    	mClusterManager = clusterer;
+    	mClusterMarker = new ClusterMarker<T>(this, clusterer.getIgnoreOnTap() );
+        addItem(item);
+    }
+
+    /**
+     * add item to cluster object
+     * @param item GeoItem object to be added.
+     */
+    public synchronized void addItem(T item){
+    	synchronized (mItems) {
+            mItems.add(item);
+		}
+//        mClusterMarker.setMarkerBitmap();
+        if(mCenter == null){
+            mCenter = item.getLatLong();
+        } else {
+            // computing the centroid
+            double lat = 0, lon = 0;
+            int n = 0;
+        	synchronized (mItems) {
+	            for (T object : mItems) {
+	            	if ( object  == null ) {
+	            		throw new NullPointerException(" object == null !");
+	            	}
+	            	if ( object.getLatLong()  == null ) {
+	            		throw new NullPointerException(" object.getLatLong() == null !");
+	            	}
+	                lat += object.getLatLong().latitude;
+	                lon += object.getLatLong().longitude;
+	                n++;
+	            }
+        	}
+            mCenter = new LatLong(lat / n, lon / n);
+        }
+    }
+
+    /**
+     * get center of the cluster.
+     * @return center of the cluster in LatLong.
+     */
+    public LatLong getLocation(){
+        return mCenter;
+    }
+
+    /**
+     * get list of GeoItem.
+     * @return list of GeoItem within cluster.
+     */
+    public synchronized List<T> getItems(){
+    	synchronized (mItems) {
+    		return mItems;
+    	}
+    }
+
+    /**
+     * clears cluster object and removes the cluster from the layers collection.
+     */
+    public void clear() {
+        if(mClusterMarker != null) {
+            Layers mapOverlays = mClusterManager.getMapView().getLayerManager().getLayers();
+            if(mapOverlays.contains(mClusterMarker)){
+                mapOverlays.remove(mClusterMarker);
+            }
+            mClusterManager = null;
+            mClusterMarker = null;
+        }
+    	synchronized (mItems) {
+    		mItems.clear();
+    	}
+    }
+
+    /**
+     * add the ClusterMarker to the Layers if is within Viewport, otherwise remove.
+     */
+    public void redraw(){
+        Layers mapOverlays = mClusterManager.getMapView().getLayerManager().getLayers();
+        if (mClusterMarker != null && !mClusterManager.getCurBounds().contains(mCenter)
+                && mapOverlays.contains(mClusterMarker)) {
+            mapOverlays.remove(mClusterMarker);
+            return;
+        }
+        if (mClusterMarker != null
+                && mapOverlays.size() > 0
+                && !mapOverlays.contains(mClusterMarker)
+                && !mClusterManager.isClustering ) {
+            mapOverlays.add(1, mClusterMarker);
+        }
+    }
+}
