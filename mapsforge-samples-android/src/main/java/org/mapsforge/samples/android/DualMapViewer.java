904a9cddc4c257e52fee5cb21f82b5804eb36e0f
Emux <devemux86@gmail.com>
2016-11-25 14:52:24 +0200

    Samples improvements #724

--- mapsforge-samples-android/src/main/java/org/mapsforge/samples/android/DualMapViewer.java
+++ mapsforge-samples-android/src/main/java/org/mapsforge/samples/android/DualMapViewer.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2010, 2011, 2012, 2013 mapsforge.org
  * Copyright 2013 - 2014 Ludwig M Brinckmann
- * Copyright 2015 devemux86
+ * Copyright 2015-2016 devemux86
  *
  * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free Software
@@ -16,7 +16,11 @@
  */
 package org.mapsforge.samples.android;
 
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.view.View;
 
 import org.mapsforge.map.android.util.AndroidPreferences;
 import org.mapsforge.map.android.util.AndroidUtil;
@@ -59,11 +63,18 @@ public class DualMapViewer extends RenderTheme4 {
         this.mapView2 = (MapView) this.findViewById(R.id.mapView2);
         this.mapView2.getModel().init(this.preferencesFacade2);
         this.mapView2.setClickable(true);
-        mapView2.getMapScaleBar().setVisible(true);
-        mapView2.setBuiltInZoomControls(hasZoomControls());
-        mapView2.getMapZoomControls().setZoomLevelMin(getZoomLevelMin());
-        mapView2.getMapZoomControls().setZoomLevelMax(getZoomLevelMax());
-        initializePosition(mapView2.getModel().mapViewPosition);
+        this.mapView2.getMapScaleBar().setVisible(true);
+        this.mapView2.setBuiltInZoomControls(hasZoomControls());
+        this.mapView2.getMapZoomControls().setZoomLevelMin(getZoomLevelMin());
+        this.mapView2.getMapZoomControls().setZoomLevelMax(getZoomLevelMax());
+        initializePosition(this.mapView2.getModel().mapViewPosition);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+            boolean hardwareAcceleration = sharedPreferences.getBoolean(SamplesApplication.SETTING_HARDWARE_ACCELERATION, true);
+            if (!hardwareAcceleration) {
+                this.mapView2.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+            }
+        }
     }
 
     protected TileCache createTileCache2() {
