905e9d48bbdb94f591e91d6a1a9aed8d6e297f79
eddiemuc <andreas-kopp@gmx.net>
2021-03-30 20:02:38 +0200

    ContentResolverResourceProvider: honor also absolute file references in theme resources (#1200)

--- mapsforge-map-android/src/main/java/org/mapsforge/map/android/rendertheme/ContentResolverResourceProvider.java
+++ mapsforge-map-android/src/main/java/org/mapsforge/map/android/rendertheme/ContentResolverResourceProvider.java
@@ -98,7 +98,9 @@ public class ContentResolverResourceProvider implements XmlThemeResourceProvider
             if (doc.isDirectory) {
                 buildCacheLevel(prefix + doc.name + "/", doc.uri);
             } else {
-                resourceUriCache.put(prefix + doc.name, doc.uri);
+                // Store both relative urls and absolute urls
+                resourceUriCache.put(XmlUtils.PREFIX_FILE + prefix + doc.name, doc.uri);
+                resourceUriCache.put(XmlUtils.PREFIX_FILE + "/" + prefix + doc.name, doc.uri);
             }
         }
     }
@@ -172,6 +174,6 @@ public class ContentResolverResourceProvider implements XmlThemeResourceProvider
             // Convert "tree uri" to a "document uri"
             dirUri = DocumentsContract.buildDocumentUriUsingTree(dirUri, DocumentsContract.getTreeDocumentId(dirUri));
         }
-        buildCacheLevel(XmlUtils.PREFIX_FILE, dirUri);
+        buildCacheLevel("", dirUri);
     }
 }
