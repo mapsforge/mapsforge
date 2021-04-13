8b006a51cf9719f6b115ed3574d632b4df8251e0
Emux <devemux86@gmail.com>
2021-04-13 13:51:11 +0300

    Map scale bar improvements (#1205)

--- mapsforge-map/src/main/java/org/mapsforge/map/scalebar/DefaultMapScaleBar.java
+++ mapsforge-map/src/main/java/org/mapsforge/map/scalebar/DefaultMapScaleBar.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2010, 2011, 2012, 2013 mapsforge.org
  * Copyright 2014 Ludwig M Brinckmann
- * Copyright 2014-2019 devemux86
+ * Copyright 2014-2021 devemux86
  * Copyright 2014 Erik Duisters
  * Copyright 2014 Christian Pesch
  *
@@ -67,6 +67,11 @@ public class DefaultMapScaleBar extends MapScaleBar {
         this.paintScaleTextStroke = createTextPaint(Color.WHITE, 2, Style.STROKE);
     }
 
+    public void setColor(int color) {
+        this.paintScaleBar.setColor(color);
+        this.paintScaleText.setColor(color);
+    }
+
     /**
      * @return the secondary {@link DistanceUnitAdapter} in use by this MapScaleBar
      */
@@ -101,7 +106,7 @@ public class DefaultMapScaleBar extends MapScaleBar {
         paint.setColor(color);
         paint.setStrokeWidth(strokeWidth * this.scale);
         paint.setStyle(style);
-        paint.setStrokeCap(Cap.SQUARE);
+        paint.setStrokeCap(Cap.ROUND);
         return paint;
     }
 
