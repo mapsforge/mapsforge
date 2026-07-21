756fe2038f6acb5fdd35bfb21ef3a77ae6b93439
Sublimis <sublimis@gmail.com>
2026-07-21 13:06:35 +0200

    Update dependencies

--- mapsforge-poi-writer/src/main/java/org/mapsforge/poi/writer/TagMappingResolver.java
+++ mapsforge-poi-writer/src/main/java/org/mapsforge/poi/writer/TagMappingResolver.java
@@ -34,9 +34,9 @@ import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 
 /**
  * This class maps a given tag (e.g. amenity=restaurant) to a certain {@link PoiCategory}.
--- mapsforge-poi-writer/src/main/java/org/mapsforge/poi/writer/XMLPoiCategoryManager.java
+++ mapsforge-poi-writer/src/main/java/org/mapsforge/poi/writer/XMLPoiCategoryManager.java
@@ -27,9 +27,9 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 
 /**
  * A {@link PoiCategoryManager} implementation that reads a category configuration from an XML file.
